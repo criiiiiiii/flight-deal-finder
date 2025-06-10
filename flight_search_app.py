1b066c1494f22d7794e94789bef5ac5c82805ad9
criiiiiiii <126253370+criiiiiiii@users.noreply.github.com>
2025-06-10 11:15:43 -0400

    Update flight_search_app.py

--- flight_search_app.py
+++ flight_search_app.py
@@ -10,14 +10,12 @@ date_from = st.date_input("Earliest Departure Date", datetime(2025, 8, 15))
 date_to = st.date_input("Latest Return Date", datetime(2025, 12, 31))
 
 if st.button("🔍 Search Deals"):
-    url = "https://kiwi-flight-search.p.rapidapi.com/flights"
+    url = "https://kiwi-com.p.rapidapi.com/api/v2/search"
 
     querystring = {
         "fly_from": origin,
         "date_from": date_from.strftime("%d/%m/%Y"),
         "date_to": date_to.strftime("%d/%m/%Y"),
-        "nights_in_dst_from": "7",
-        "nights_in_dst_to": "14",
         "adults": "2",
         "children": "2",
         "curr": "USD",
@@ -26,8 +24,8 @@ if st.button("🔍 Search Deals"):
     }
 
     headers = {
-        "X-RapidAPI-Key": "215a6826f2mshc7e99c81ebbe6e0p129a86jsn13e40defdfae",  # your actual key
-        "X-RapidAPI-Host": "kiwi-flight-search.p.rapidapi.com"
+        "X-RapidAPI-Key": "215a6826f2mshc7e99c81ebbe6e0p129a86jsn13e40defdfae",
+        "X-RapidAPI-Host": "kiwi-com.p.rapidapi.com"
     }
 
     response = requests.get(url, headers=headers, params=querystring)
